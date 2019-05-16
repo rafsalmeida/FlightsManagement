5a4d41abd657763e2020ab8a3ce7a3dfdbe24cdd
paulomvcustodio <50708199+paulomvcustodio@users.noreply.github.com>
2019-05-16 16:01:09 +0100

    Update edit.blade.php

--- resources/views/aeronaves/edit.blade.php
+++ resources/views/aeronaves/edit.blade.php
@@ -13,6 +13,15 @@
     <div class="container" style="padding-top: 15px">
     	@yield('form')
     </div>
+
+     <div>
+       @dump($valores)
+       @foreach($valores as $valor)
+
+{{$valor->unidade_conta_horas}} - {{$valor->minutos}} {{$valor->preco}} <br>
+       @endforeach 
+
+    </div>
     <div class="form-group" style="padding-left: 15px;">
         <button type="submit" class="btn btn-success" name="ok">Submeter</button>
         <button type="submit" class="btn btn-default" name="cancel">Cancelar</button>
