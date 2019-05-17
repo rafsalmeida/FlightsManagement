39818bb050d629c7e52bb8330b59496e9a63ec38
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-17 16:01:04 +0100

    Update TipoLicenca.php

--- app/TipoLicenca.php
+++ app/TipoLicenca.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class TipoLicenca extends Model
 {
     protected $table = "tipos_licencas";
+    
+    public function socio()
+	{
+		return $this->hasMany('App\Socio');
+	}
 }
