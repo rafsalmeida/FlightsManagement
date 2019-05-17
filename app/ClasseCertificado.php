bc205bfd5851bd0afc8eb4a91d8476d9dce0c947
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-17 17:45:25 +0100

    Update ClasseCertificado.php

--- app/ClasseCertificado.php
+++ app/ClasseCertificado.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class ClasseCertificado extends Model
 {
     protected $table = "classes_certificados";
+
+    public function socio()
+	{
+		return $this->hasMany('App\Socio', 'classe_certificado', 'code');
+	}
 }
+
+
