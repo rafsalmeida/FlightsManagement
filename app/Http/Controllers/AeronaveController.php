6d1bd966cdd0bd7ad2dcf16484b6274aee3fd672
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-18 16:55:58 +0100

    Update AeronaveController.php

--- app/Http/Controllers/AeronaveController.php
+++ app/Http/Controllers/AeronaveController.php
@@ -13,6 +13,12 @@ class AeronaveController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $aeronaves = Aeronave::All();
