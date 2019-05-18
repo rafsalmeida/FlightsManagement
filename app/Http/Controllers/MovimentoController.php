a3be61ef4c72d32d33837c739a4f56cf79ab8514
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-18 16:56:01 +0100

    Update MovimentoController.php

--- app/Http/Controllers/MovimentoController.php
+++ app/Http/Controllers/MovimentoController.php
@@ -12,6 +12,13 @@ class MovimentoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    
     public function index()
     {
         $movimentos = Movimento::paginate(15);
