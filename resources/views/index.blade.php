4e092d6f7f7f1e956723ec94820138aba8357dcc
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-18 16:42:20 +0100

    Create index.blade.php
    farta

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('master')
+@section('title', "FlightClub")
+@section('content')
+
+<div style="text-align: center; padding-top: 10vh">
+	<i class="fas fa-plane fa-5x fa-spin"></i>
+	<h4>Bem-vindo ao Flight Club</h4>
+</div>
+@endsection
