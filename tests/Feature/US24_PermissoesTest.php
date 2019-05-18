ca3083e13ce755de14a13fc8f366e7011c751230
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-18 16:45:21 +0100

    Update US24_PermissoesTest.php

--- tests/Feature/US24_PermissoesTest.php
+++ tests/Feature/US24_PermissoesTest.php
@@ -8,40 +8,65 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class US24_PermissoesTest extends USTestBase
 {
-    protected $userToSimulate;
     protected function setUp(): void
     {
         parent::setUp();
         $this->seedNormalUsers();
         $this->seedDirecaoUser();
-        $this->userToSimulate = $this->normalUser;
     }
 
-    public function testGetSociosEditAcessivelDirecao()
-    {
-        $this->actingAs($this->direcaoUser)->get("/socios/". $this->normalUser->id.'/edit')
-                ->assertAuthorized('GET', "/socios/". $this->normalUser->id.'/edit', 'Um sócio da direção não consegue ver os dados de perfil de um sócio!');
-    }
 
-    public function testGetSociosEditInacessivelOutros()
-    {
-        $this->actingAs($this->normalUser2)->get("/socios/". $this->normalUser->id.'/edit')
-                ->assertUnauthorized('GET', "/socios/". $this->normalUser->id.'/edit', 'Um sócio "normal" consegue ver os dados de perfil de outro sócio!');
-    }
-
-    public function testProtecaoAlterarSocioDirecao()
+    public function testProtecaoAlterarSociosParaAnonimos()
     {
         $newdata = ["nome_informal" => "Valor Válido"];
         $requestData = array_merge($this->getRequestArrayFromUser($this->normalUser), $newdata);
-        $this->actingAs($this->direcaoUser)->put('/socios/'. $this->normalUser->id, $requestData)
-            ->assertAuthorized('PUT', "/socios/". $this->normalUser->id, 'Um sócio da direção não consegue alterar os dados de perfil de um sócio!');
+        $this->put('/socios/'. $this->normalUser->id, $requestData)
+            ->assertUnauthorized('PUT', "/socios/". $this->normalUser->id, 'Anónimos conseguem alterar dados de perfil de um sócio!');
     }
 
-    public function testProtecaoAlterarSocioOutros()
+    public function testProtecaoAlterarSociosParaOutroSocio()
     {
         $newdata = ["nome_informal" => "Valor Válido"];
         $requestData = array_merge($this->getRequestArrayFromUser($this->normalUser), $newdata);
         $this->actingAs($this->normalUser2)->put('/socios/'. $this->normalUser->id, $requestData)
-            ->assertUnauthorized('PUT', "/socios/". $this->normalUser->id, 'Um sócio "normal" consegue alterar os dados de perfil de outro sócio!');
+            ->assertUnauthorized('PUT', "/socios/". $this->normalUser->id, 'Sócios normais conseguem alterar dados de perfil de outro sócio!');
     }
+
+    public function testProtecaoCriarSocio()
+    {
+        $uniqueNum = 3584943;
+        $originalData = $this->getRequestArrayFromUser($this->normalUser);
+        $newdata = [
+            "name" => "Novo Nome Para Normal User XX",
+            "nome_informal" => "Novo Informal " . $uniqueNum,
+            "email" => $uniqueNum . "@nanaemail.pt",
+            "data_nascimento" => $this->format_date_input("1965-08-23"),
+            "nif" => "999999999",
+            "telefone" => "999999999",
+            "endereco" => "Rua para testes",
+            "num_socio" => $uniqueNum,         
+            "ativo" => 1,
+            "quota_paga" => 1,
+            "sexo" => "M",
+            "tipo_socio" => "NP",
+            "direcao" => 1,
+            "instrutor" => 0,
+            "aluno" => 0
+        ];
+
+        $requestData = array_merge($originalData, $newdata);
+
+        $this->actingAs($this->normalUser)
+            ->post('/socios', $requestData)
+            ->assertUnauthorized('POST', "/socios", 'Sócios normais conseguem criar novos sócios!');
+    }
+
+    public function testProtecaoApagarSocio()
+    {
+        $this->actingAs($this->normalUser)
+                ->delete('/socios/'. $this->normalUser->id)
+                ->assertUnauthorized('DELETE', '/socios/'. $this->normalUser->id, 'Sócios normais conseguem apagar sócios!');        
+    }
+
+
 }
