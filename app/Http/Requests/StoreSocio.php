f1df400ac184c7fc69c2c61a0c5524da92b8db8b
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-19 23:40:24 +0100

    Update StoreSocio.php

--- app/Http/Requests/StoreSocio.php
+++ app/Http/Requests/StoreSocio.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\validation\Rule;
 
 class StoreSocio extends FormRequest
 {
@@ -24,19 +25,19 @@ class StoreSocio extends FormRequest
     public function rules()
     {
         return [
-            "num_socio" => "required|integer",
-            "name" => "required|string|alpha|max:255",
-            "nome_informal" => "required|string|alpha_dash|max:40",
-            "email" => "required|email|max:255",
-            "nif" => "digits:9|nullable",
+            "num_socio" => "required|integer|regex:/^\d{1,11}$/|".Rule::unique('users')->ignore($this->id),
+            "name" => "required|string|max:255|regex:/^[\pL\s]+$/u",
+            "nome_informal" => "required|alpha_dash|max:40",
+            "email" => "required|email|max:255|".Rule::unique('users')->ignore($this->id),
+            "nif" => "digits:9|nullable|".Rule::unique('users')->ignore($this->id),
             "data_nascimento" => "required|date",
-            "telefone" => "string|nullable",
+            "telefone" => "string|max:20|nullable",
             "endereco" => "string|nullable",
             "num_licenca" => "string|nullable",
             "validade_licenca" => "date|nullable",
             "num_certificado" => "string|nullable",
             "validade_certificado" => "date|nullable",
-            "foto_url" => "file|image|nullable",
+            "foto_url" => "image|mimes:jpeg,png,jpg,gif,svg|max:2048|nullable",
         ];
     }
 }
