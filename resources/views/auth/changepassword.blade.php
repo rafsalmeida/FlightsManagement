8c5916e4284b8ad33e577b020fa57eae696fd174
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-20 17:39:41 +0100

    Update changepassword.blade.php

--- resources/views/auth/changepassword.blade.php
+++ resources/views/auth/changepassword.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('master')
 
 @section('content')
-    <div class="container">
+    <div class="container" style="padding-top: 10px">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Reset Password') }}</div>
+                    <div class="card-header">{{ __('Alterar Password') }}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('password.change') }}">
@@ -15,7 +15,7 @@
                             <input type="hidden" name="token" value="{{csrf_token()}}">
 
                             <div class="form-group row">
-                                <label for="old_password" class="col-md-4 col-form-label text-md-right">{{ __('old_password') }}</label>
+                                <label for="old_password" class="col-md-4 col-form-label text-md-right">{{ __('Password antiga') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="old_password" type="password" class="form-control{{ $errors->has('old_password') ? ' is-invalid' : '' }}" name="old_password" required>
@@ -43,7 +43,7 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Password') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
@@ -53,7 +53,7 @@
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        {{ __('Reset Password') }}
+                                        {{ __('Mudar Password') }}
                                     </button>
                                 </div>
                             </div>
