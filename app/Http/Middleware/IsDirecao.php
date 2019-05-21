f267b66bad377b54ef65dda51179845c4463dede
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-21 17:08:23 +0100

    Update IsDirecao.php

--- app/Http/Middleware/IsDirecao.php
+++ app/Http/Middleware/IsDirecao.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
-
+use \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Closure;
 
 class IsDirecao
@@ -15,7 +15,7 @@ class IsDirecao
      */
     public function handle($request, Closure $next)
     {
-        if ($request->socio() && $request->socio()->direcao == 1) {
+        if ($request->user() && $request->user()->direcao == 1) {
             return $next($request);
         }
 
