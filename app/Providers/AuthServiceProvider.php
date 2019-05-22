19650b662f7e925068200a763c5f644def916f31
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-23 00:12:59 +0100

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Socio;
+use App\Policies\SocioPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -14,6 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
+        Socio::class => SocioPolicy::class,
     ];
 
     /**
