e96655d6fd99d5a105973f86f687118235624c2d
jmczimas <50708539+jmczimas@users.noreply.github.com>
2019-05-22 16:48:57 +0100

    Update Movimento.php

--- app/Movimento.php
+++ app/Movimento.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Movimento extends Model
 {
     protected $table = 'movimentos';
-    protected $fillable = ['aeronave', 'num_diario', 'num_servico', 'piloto_id','aerodromo_partida','aerodromo_chegada','num_aterragens','num_descolagens','num_pessoas','conta_horas_inicio','conta_horas_fim','num_recibo'];
+    protected $fillable = ['data','hora_descolagem', 'hora_aterragem', 'aeronave', 'num_diario', 'num_servico', 'piloto_id','aerodromo_partida','aerodromo_chegada','num_aterragens','num_descolagens','num_pessoas','conta_horas_inicio','conta_horas_fim','num_recibo'];
 }
