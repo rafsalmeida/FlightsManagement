920b1bc1b7f46964e273accbdbd92d2447b71ead
paulomvcustodio <50708199+paulomvcustodio@users.noreply.github.com>
2019-05-23 14:00:39 +0100

    Update Aeronave.php

--- app/Aeronave.php
+++ app/Aeronave.php
@@ -14,7 +14,7 @@ class Aeronave extends Model
     protected $fillable = ['matricula', 'marca', 'modelo', 'num_lugares','conta_horas','preco_hora'];
 
     
-    public function aeronaveValores(){
+    public function valores(){
     	return $this->hasMany('App\AeronaveValor', 'matricula');
     }
 }
