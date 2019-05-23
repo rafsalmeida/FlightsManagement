6f61786f5044115a6e7e03133adc398674812bbc
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-23 12:38:16 +0100

    Create UserPolicy.php

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Socio;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function view(User $socioLogged, Socio $socio){
+        if($socioLogged->direcao){
+            return true;
+        }
+
+        if($socioLogged->id === $socio->id){
+            return true;
+        }
+
+        return false;
+
+    }
+}
