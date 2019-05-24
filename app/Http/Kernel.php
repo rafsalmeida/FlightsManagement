f1cbcd101968a5ad20c36d64f057d969a2a70993
paulomvcustodio <50708199+paulomvcustodio@users.noreply.github.com>
2019-05-24 12:43:26 +0100

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'direcao' => \App\Http\Middleware\IsDirecao::class,
+        'ativo' =>  \App\Http\Middleware\IsAtivo::class,
         'passwd_changed' => \App\Http\Middleware\IsPasswdChanged::class,
     ];
 
