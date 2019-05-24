60e524fdb93bee9a8b6910028c71d3fdcff68476
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-24 11:48:34 +0100

    Update User.php

--- app/User.php
+++ app/User.php
@@ -11,16 +11,8 @@ class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
     use SoftDeletes;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
+    protected $table = 'users';
+    use SoftDeletes;
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -38,4 +30,24 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $fillable = ['num_socio', 'name', 'nome_informal', 'email','nif','sexo','data_nascimento','telefone','endereco','num_licenca','validade_licenca','num_certificado','validade_certificado'];
+
+    public function tipoLicenca()
+    {
+        return $this->belongsTo('App\TipoLicenca', 'tipo_licenca','code');
+
+    }
+
+    public function classeCertificado()
+    {
+        return $this->belongsTo('App\ClasseCertificado', 'classe_certificado','code');
+
+    }
+
+    public function movimentos(){
+        return $this->hasMany('App\Movimentos');
+    }
+   
+    
 }
