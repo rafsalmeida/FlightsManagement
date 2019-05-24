fb2991230c9d6da787a788bac90528647109356d
paulomvcustodio <50708199+paulomvcustodio@users.noreply.github.com>
2019-05-24 18:07:40 +0100

    Update list.blade.php

--- resources/views/aeronaves/list.blade.php
+++ resources/views/aeronaves/list.blade.php
@@ -1,8 +1,12 @@
 @extends('master')
 @section('title', "Lista de Aeronaves")
 @section('content')
-<div style="padding: 2vh 0"><a class="btn btn-primary" href="{{ route('aeronaves.create') }}">Adicionar Aeronave</a></div>
+@can('is-direcao', Auth::user())
+<div style="padding-top: 10px"><a class="btn btn-primary" href="{{ route('aeronaves.create') }}">Adicionar Aeronave</a></div>
+@endcan
+<div style="padding-top: 10px">
 @if (count($aeronaves))
+
     <table class="table table-bordered shadow p-3 mb-5 bg-white rounded">
     <thead class="thead-light">
         <tr>
@@ -24,7 +28,9 @@
             <td>{{ $aeronave->num_lugares }}</td>
             <td>{{ $aeronave->conta_horas/10}} horas</td>
             <td>{{ $aeronave->preco_hora }}€</td>
+            @can('is-direcao', Auth::user())
             <td>
+              
                 <div style="text-align: center; margin: auto">
                 <a class="btn btn-sm btn-xs btn-primary rounded-pill" style="width: 100%" href="{{action('AeronaveController@edit', $aeronave->matricula)}}">Editar</a>
                 <form action="{{action('AeronaveController@destroy', $aeronave->matricula)}}" method="POST" role="form" class="inline">
@@ -32,12 +38,15 @@
                     @method('delete')
                     <input type="hidden" name="aeronave_matricula" value="{{ $aeronave->matricula }}">
                     {!! Form::button('<i class="fas fa-exclamation-triangle"></i> Apagar', ['type' => 'submit', 'class' => 'btn btn-sm btn-xs btn-danger rounded-pill', 'style' => 'width: 100%', 'onclick' => "return confirm('Tem a certeza que quer apagar?')"]) !!}
+                   
                 </form>
             </div>
             </td>
+             @endcan
         </tr>
     @endforeach
     </table>
+</div>
 
 
 @else
