e71f1dc61a1fbcfabb6aedd1ea6b2f3087874dc8
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-26 21:51:51 +0100

    Update User.php

--- app/User.php
+++ app/User.php
@@ -50,7 +50,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function aeronave()
     {
-        return $this->belongsToMany('App\Aeronave','aeronaves_pilotos', 'matricula', 'piloto_id');
+        return $this->belongsToMany('App\Aeronave','aeronaves_pilotos', 'piloto_id', 'matricula');
     }
 
    
