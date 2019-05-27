d4859bf5659671110144873c6dfd532159439109
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-27 15:59:46 +0100

    Update Movimento.php

--- app/Movimento.php
+++ app/Movimento.php
@@ -10,7 +10,11 @@ class Movimento extends Model
     protected $fillable = ['data','hora_descolagem', 'hora_aterragem', 'aeronave', 'num_diario', 'num_servico', 'piloto_id','aerodromo_partida','aerodromo_chegada','num_aterragens','num_descolagens','num_pessoas','conta_horas_inicio','conta_horas_fim','num_recibo'];
 
     public function piloto(){
-    	return $this->belongsTo('App\User');
+    	return $this->belongsTo('App\User', 'piloto_id', 'id');
+    }
+
+    public function instrutor(){
+    	return $this->belongsTo('App\User', 'instrutor_id', 'id');
     }
 
     public function aeronaves(){
