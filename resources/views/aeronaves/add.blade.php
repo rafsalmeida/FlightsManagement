a80e22eaa3f9b241e08874adb948ae227268be16
paulomvcustodio <50708199+paulomvcustodio@users.noreply.github.com>
2019-05-28 22:06:02 +0100

    Update add.blade.php

--- resources/views/aeronaves/add.blade.php
+++ resources/views/aeronaves/add.blade.php
@@ -6,9 +6,9 @@
     @include('partials.errors')
 @endif
 <form method="POST" action="{{ action('AeronaveController@store')}}"  class="form-group">
-	@csrf
+  @csrf
     <div class="container" style="padding-top: 15px">
-    	@yield('form')
+      @yield('form')
     </div>
     <div class="w-100 p-3  container" >
        <table class="table table-bordered  table table-sm shadow p-3 mb-5 bg-white rounded">
@@ -20,28 +20,28 @@
         </tr>
         </thead>
     <tbody>
-  		@for ($i = 1; $i < 11; $i++)
-	
+      @for ($i = 1; $i < 11; $i++)
+  
         <tr>
             <td>{{$i}} </td>
-            <td><div class="form-group">
-    			<input type="text" class="form-control" name="tempos[]" id="minuto"
-        				placeholder="Minutos"/>
-   			             <!-- Verificar-->
-   			             @if ($errors->has('tempos'))
-        				<em>{{ $errors->first('tempos') }}</em>
-   						 @endif
-			</div> </td>
+            <td>
+          <input type="text" class="form-control" name="tempos[{{$i}}]" id="minuto"
+                placeholder="Minutos"/>
+                     <!-- Verificar-->
+                     @if ($errors->has('tempos'))
+                <em>{{ $errors->first('tempos') }}</em>
+               @endif
+      </div> </td>
 
             <td>
-            	<div class="form-group">
-    			<input type="text" class="form-control" name="precos[]" id="preco_minuto"
-        				placeholder="Precos"/>
-   			             <!-- Verificar-->
-   			             @if ($errors->has('precos'))
-        				<em>{{ $errors->first('precos') }}</em>
-   						 @endif
-			</div> 
+              <div class="form-group">
+          <input type="text" class="form-control" name="precos[{{$i}}]" id="preco_minuto"
+                placeholder="Precos"/>
+                     <!-- Verificar-->
+                     @if ($errors->has('precos'))
+                <em>{{ $errors->first('precos') }}</em>
+               @endif
+      </div> 
             </td>
            
         </tr>
