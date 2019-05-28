5b6bd1d842ed62990ce29334a38296c42640616b
jmczimas <50708539+jmczimas@users.noreply.github.com>
2019-05-28 19:34:04 +0100

    Update list.blade.php

--- resources/views/movimentos/list.blade.php
+++ resources/views/movimentos/list.blade.php
@@ -7,7 +7,7 @@
     <div class="col" style="padding-top: 55px; padding-left: 0px; position: relative; float: 
         left"><a class="btn btn-secondary" href="{{ route('movimentos.estatisticas') }}">Estatísticas</a></div>
     <div class="col" style="padding-top: 55px; padding-left: 0px; position: relative; float: 
-        left"><a class="btn btn-secondary" href="{{action('MovimentoController@confirmar')}}">Confirmar Voos</a></div>
+        left"><a class="btn btn-secondary" href="#">Confirmar Voos</a></div>
     <div class="form-group" style="padding-top: 30px; padding-right: 10px; float: right;">            
         <form  method="GET" action="{{action('MovimentoController@index')}}" id="pesquisarMovimento">
             <div class="form-row ">
