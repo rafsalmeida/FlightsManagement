c32fc4f638e32ad6f966074941f7d4f8848e7253
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-29 10:39:50 +0100

    Update StoreMovimento.php

--- app/Http/Requests/StoreMovimento.php
+++ app/Http/Requests/StoreMovimento.php
@@ -26,25 +26,25 @@ class StoreMovimento extends FormRequest
     public function rules()
     {
         return [
-        'data' => 'required',
-        'hora_descolagem' => 'required',
-        'hora_aterragem' => 'required',
+        'data' => 'required|date_format:"Y-m-d"|before_or_equal:today',
+        'hora_descolagem' => 'required|date_format:"H:i"',
+        'hora_aterragem' => 'required|date_format:"H:i"|after:hora_descolagem',
         'aeronave' => 'required|string|exists:aeronaves,matricula',
-        'natureza' => 'required',
+        'natureza' => 'required|in:I,T,E',
         'num_diario' => 'required|integer',
         'num_servico' => 'required|integer',
         'piloto_id' => 'required|integer|'.Rule::exists('aeronaves_pilotos','piloto_id','matricula',$this->aeronave),
         'aerodromo_partida' => 'required|string|exists:aerodromos,code',
         'aerodromo_chegada' => 'required|string|exists:aerodromos,code',
-        'num_aterragens' => 'required|integer',
-        'num_descolagens' => 'required|integer',
-        'num_pessoas' => 'required|integer',
-        'conta_horas_inicio' => 'required|integer',
+        'num_aterragens' => 'required|integer|min:0',
+        'num_descolagens' => 'required|integer|min:0',
+        'num_pessoas' => 'required|integer|min:0',
+        'conta_horas_inicio' => 'required|integer|min:0',
         'conta_horas_fim' => 'required|integer|min:'.$this->conta_horas_inicio,
-        'num_recibo' => 'required|integer',
+        'num_recibo' => 'required|integer|min:0',
         'instrutor_id' => 'nullable|required_if:natureza,I|'.Rule::exists('aeronaves_pilotos','piloto_id','matricula',$this->aeronave),
         'tipo_instrucao' => 'required_if:natureza,I',
-        'modo_pagamento' => 'required'
+        'modo_pagamento' => 'required|in:N,T,M,P',
         ];
     }
 }
