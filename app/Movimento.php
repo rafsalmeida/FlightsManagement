a8933dab4aa1a6f1263096cf084217e2bd978108
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-29 16:29:34 +0100

    Update Movimento.php

--- app/Movimento.php
+++ app/Movimento.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Movimento extends Model
 {
     protected $table = 'movimentos';
-    protected $fillable = ['data','hora_descolagem', 'hora_aterragem', 'aeronave', 'num_diario', 'num_servico', 'piloto_id','aerodromo_partida','aerodromo_chegada','num_aterragens','num_descolagens','num_pessoas','conta_horas_inicio','conta_horas_fim','num_recibo'];
+    protected $fillable = ['data','hora_descolagem', 'hora_aterragem', 'aeronave', 'num_diario', 'num_servico', 'piloto_id','aerodromo_partida','aerodromo_chegada','num_aterragens','num_descolagens','num_pessoas','conta_horas_inicio','conta_horas_fim','num_recibo','num_licenca_piloto', 'validade_licenca_piloto', 'tipo_licenca_piloto','num_certificado_piloto','validade_certificado_piloto', 'classe_certificado_piloto','num_licenca_instrutor','validade_licenca_instrutor','tipo_licenca_instrutor','num_licenca_instrutor','validade_licenca_instrutor','classe_certificado_instrutor','natureza','tempo_voo','preco_voo','modo_pagamento','observacoes','confirmado','tipo_instrucao','instrutor_id','tipo_conflito','justificacao_conflito'];
 
     public function piloto(){
     	return $this->belongsTo('App\User', 'piloto_id', 'id');
@@ -29,11 +29,5 @@ class Movimento extends Model
         return $this->belongsTo('App\Aerodromo', 'code', 'aerodromo_chegada');
     }
 
-    public function calcTempoVoo($partida, $chegada){
-        
 
-
-        
-        return ($partida-$chegada)*6;
-    }
 }
