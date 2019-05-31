c5996b0c369e0d9c9b4ca5e845e73a1e89dac4fd
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-05-31 13:33:33 +0100

    Create unsuccess.blade.php

---
+++ resources/views/partials/unsuccess.blade.php
@@ -0,0 +1,4 @@
+<div class="alert alert-warning" style="margin-top: 2vh">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    {{session('unsuccess')}}
+</div>
