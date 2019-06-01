0a5d9b43b475f4ef0d0d55c9745704f38808b8b8
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-06-01 11:34:52 +0100

    Update add.blade.php

--- resources/views/movimentos/add.blade.php
+++ resources/views/movimentos/add.blade.php
@@ -12,7 +12,8 @@
     </div>
     <div class="form-group" style="padding-left: 15px;">
         <button type="submit" class="btn btn-success" name="ok">Adicionar</button>
-        <button type="submit" class="btn btn-default" name="cancel">Cancelar</button>
+        <a class="btn btn-default" href="{{action('MovimentoController@index')}}" name="cancel">Cancelar</a>
     </div>
 </form>
+
 @endsection
