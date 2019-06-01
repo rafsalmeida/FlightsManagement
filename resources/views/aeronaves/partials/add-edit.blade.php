08eb64f0c82766d1af642979ac524e6ffeae9f5f
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-06-01 22:25:05 +0100

    Update add-edit.blade.php

--- resources/views/aeronaves/partials/add-edit.blade.php
+++ resources/views/aeronaves/partials/add-edit.blade.php
@@ -6,7 +6,7 @@
         name="matricula" id="inputMatricula"
         placeholder="Matricula" value="@if(isset($aeronave)){{old('matricula', $aeronave->matricula)}}@endif" />
     @if ($errors->has('matricula'))
-        <em>{{ $errors->first('matricula') }}</em>
+    <em>{{ $errors->first('matricula') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -16,7 +16,7 @@
         name="marca" id="inputMarca"
         placeholder="Marca" value="@if(isset($aeronave)){{ old('marca', $aeronave->marca) }}@endif" />
     @if ($errors->has('marca'))
-        <em>{{ $errors->first('marca') }}</em>
+    <em>{{ $errors->first('marca') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -26,7 +26,7 @@
         name="modelo" id="inputModelo"
         placeholder="Modelo" value="@if(isset($aeronave)){{ old('modelo', $aeronave->modelo) }}@endif" />
     @if ($errors->has('modelo'))
-        <em>{{ $errors->first('modelo') }}</em>
+    <em>{{ $errors->first('modelo') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -36,7 +36,7 @@
         name="num_lugares" id="inputLugares"
         placeholder="NumLugares" value="@if(isset($aeronave)){{ old('num_lugares', $aeronave->num_lugares) }}@endif" />
     @if ($errors->has('num_lugares'))
-        <em>{{ $errors->first('num_lugares') }}</em>
+    <em>{{ $errors->first('num_lugares') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -46,7 +46,7 @@
         name="conta_horas" id="inputContaHoras"
         placeholder="ContaHoras" value="@if(isset($aeronave)){{ old('conta_horas', $aeronave->conta_horas) }}@endif" />
     @if ($errors->has('conta_horas'))
-        <em>{{ $errors->first('conta_horas') }}</em>
+    <em>{{ $errors->first('conta_horas') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -56,7 +56,7 @@
         name="preco_hora" id="inputPrecoHora"
         placeholder="PrecoHora" value="@if(isset($aeronave)){{ old('preco_hora', $aeronave->preco_hora) }}@endif" />
     @if ($errors->has('preco_hora'))
-        <em>{{ $errors->first('preco_hora') }}</em>
+    <em>{{ $errors->first('preco_hora') }}</em>
     @endif
 </div>
 @endsection
