5e026cd0fc5923fbb53a4a82a98d0636aa5ff757
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-06-01 22:25:37 +0100

    Update add-edit.blade.php

--- resources/views/movimentos/partials/add-edit.blade.php
+++ resources/views/movimentos/partials/add-edit.blade.php
@@ -7,16 +7,17 @@
         name="data" id="inputData"
         placeholder="Data" value="@if(isset($movimento )){{old('data', $movimento->data)}}@else{{ old('data') }}@endif" />
     @if ($errors->has('data'))
-        <em>{{ $errors->first('data') }}</em>
+    <em>{{ $errors->first('data') }}</em>
     @endif
+</div>
 <div class="form-group">
     <label for="inputHoraDescolagem">Hora Descolagem</label>
     <input
         type="time" class="form-control"
         name="hora_descolagem" id="inputHoraDescolagem"
-        placeholder="aaaa-mm-dd hh:mm:ss" value="@if(isset($movimento)){{ old('hora_descolagem', date('H:i',strtotime($movimento->hora_descolagem))) }}@else{{ old('hora_descolagem') }}@endif" />
+        placeholder="aaaa-mm-dd hh:mm:ss" value="@if(isset($movimento)){{ old('hora_descolagem', date('H:i',strtotime($movimento->hora_descolagem))) }}@else{{ date('H:i',strtotime(old('hora_descolagem'))) }}@endif" />
     @if ($errors->has('hora_descolagem'))
-        <em>{{ $errors->first('hora_descolagem') }}</em>
+    <em>{{ $errors->first('hora_descolagem') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -24,18 +25,18 @@
     <input
         type="time" class="form-control"
         name="hora_aterragem" id="inputHoraAterragem"
-        placeholder="aaaa-mm-dd hh:mm:ss" value="@if(isset($movimento)){{ old('hora_aterragem', date('H:i',strtotime($movimento->hora_aterragem))) }}@else{{  old('hora_aterragem')  }}@endif" />
+        placeholder="aaaa-mm-dd hh:mm:ss" value="@if(isset($movimento)){{ old('hora_aterragem', date('H:i',strtotime($movimento->hora_aterragem))) }}@else{{  date('H:i',strtotime(old('hora_aterragem')))  }}@endif" />
     @if ($errors->has('hora_aterragem'))
-        <em>{{ $errors->first('hora_aterragem') }}</em>
+    <em>{{ $errors->first('hora_aterragem') }}</em>
     @endif
 </div>
 <div class="form-group">
     <form id="getMatricula">
         <label >Matrícula Aeronave</label>
         @if(isset($movimento->aeronave))
-            {!! Form::select('aeronave', $aeronaves, $movimento->aeronave, ['class' => 'form-control conta-horas', 'id' => 'matricula']) !!}
+        {!! Form::select('aeronave', $aeronaves, $movimento->aeronave, ['class' => 'form-control conta-horas', 'id' => 'matricula']) !!}
         @else
-            {!! Form::select('aeronave', $aeronaves, old('aeronave'), ['class' => 'form-control conta-horas', 'id' => 'matricula'])!!}
+        {!! Form::select('aeronave', $aeronaves, old('aeronave'), ['class' => 'form-control conta-horas', 'id' => 'matricula'])!!}
         @endif
 
 </div>
@@ -46,7 +47,7 @@
         name="num_diario" id="inputNDiario"
         placeholder="NDiario" value="@if(isset($movimento)){{ old('num_diario', $movimento->num_diario) }}@else{{ old('num_diario') }}@endif" />
     @if ($errors->has('num_diario'))
-        <em>{{ $errors->first('num_diario') }}</em>
+    <em>{{ $errors->first('num_diario') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -56,7 +57,7 @@
         name="num_servico" id="inputNServico"
         placeholder="NServico" value="@if(isset($movimento)){{ old('num_servico', $movimento->num_servico) }}@else{{ old('num_servico') }}@endif" />
     @if ($errors->has('num_servico'))
-        <em>{{ $errors->first('num_servico') }}</em>
+    <em>{{ $errors->first('num_servico') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -66,7 +67,7 @@
         name="piloto_id" id="inputPiloto"
         placeholder="Piloto" value="@if(isset($movimento)){{ old('piloto_id', $movimento->piloto_id) }}@else{{ Auth::user()->id }}@endif" />
     @if ($errors->has('piloto_id'))
-        <em>{{ $errors->first('piloto_id') }}</em>
+    <em>{{ $errors->first('piloto_id') }}</em>
     @endif
 </div>
 
@@ -74,29 +75,29 @@
     <label for="inputType">Natureza do Voo</label>
     <div>
         @if(isset($movimento))
-            {{ Form::select('natureza', [null => 'Natureza (Selecione)'] + array('T' => 'Treino', 'I' => 'Instrução', 'E' => 'Especial'), $movimento->natureza, ['id' => 'natureza', 'class' => 'form-control']) }}
+        {{ Form::select('natureza', [null => 'Natureza (Selecione)'] + array('T' => 'Treino', 'I' => 'Instrução', 'E' => 'Especial'), $movimento->natureza, ['id' => 'natureza', 'class' => 'form-control']) }}
         @else
-            {{ Form::select('natureza', [null => 'Natureza (Selecione)'] +  array('T' => 'Treino', 'I' => 'Instrução', 'E' => 'Especial'), old('natureza'), ['id' => 'natureza', 'class' => 'form-control'])}}
+        {{ Form::select('natureza', [null => 'Natureza (Selecione)'] +  array('T' => 'Treino', 'I' => 'Instrução', 'E' => 'Especial'), old('natureza'), ['id' => 'natureza', 'class' => 'form-control'])}}
         @endif
     </div>
 </div>
 
 <div class="form-group">
     <label >Aerodromo de Partida</label>
-        @if(isset($movimento->aerodromo_partida))
-            {!! Form::select('aerodromo_partida', $aerodromos, $movimento->aerodromo_partida, ['class' => 'form-control']) !!}
-        @else
-            {!! Form::select('aerodromo_partida', $aerodromos, old('aerodromo_partida'), ['class' => 'form-control'])!!}
-        @endif
+    @if(isset($movimento->aerodromo_partida))
+    {!! Form::select('aerodromo_partida', $aerodromos, $movimento->aerodromo_partida, ['class' => 'form-control']) !!}
+    @else
+    {!! Form::select('aerodromo_partida', $aerodromos, old('aerodromo_partida'), ['class' => 'form-control'])!!}
+    @endif
 </div>
 
 <div class="form-group">
     <label >Aerodromo de Chegada</label>
-        @if(isset($movimento->aerodromo_chegada))
-            {!! Form::select('aerodromo_chegada', $aerodromos, $movimento->aerodromo_chegada, ['class' => 'form-control']) !!}
-        @else
-            {!! Form::select('aerodromo_chegada', $aerodromos, old('aerodromo_chegada'), ['class' => 'form-control'])!!}
-        @endif
+    @if(isset($movimento->aerodromo_chegada))
+    {!! Form::select('aerodromo_chegada', $aerodromos, $movimento->aerodromo_chegada, ['class' => 'form-control']) !!}
+    @else
+    {!! Form::select('aerodromo_chegada', $aerodromos, old('aerodromo_chegada'), ['class' => 'form-control'])!!}
+    @endif
 </div>
 
 <div class="form-group">
@@ -106,7 +107,7 @@
         name="num_aterragens" id="inputNAterragens"
         placeholder="NAterragens" value="@if(isset($movimento)){{ old('num_aterragens', $movimento->num_aterragens) }}@else{{ old('num_aterragens') }}@endif" />
     @if ($errors->has('num_aterragens'))
-        <em>{{ $errors->first('num_aterragens') }}</em>
+    <em>{{ $errors->first('num_aterragens') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -116,7 +117,7 @@
         name="num_descolagens" id="inputNDescolagens"
         placeholder="NDescolagens" value="@if(isset($movimento)){{ old('num_descolagens', $movimento->num_descolagens) }}@else{{ old('num_descolagens') }}@endif" />
     @if ($errors->has('num_descolagens'))
-        <em>{{ $errors->first('num_descolagens') }}</em>
+    <em>{{ $errors->first('num_descolagens') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -126,7 +127,7 @@
         name="num_pessoas" id="inputNPessoas"
         placeholder="NPessoas" value="@if(isset($movimento)){{ old('num_pessoas', $movimento->num_pessoas) }}@else{{ old('num_pessoas') }}@endif" />
     @if ($errors->has('num_pessoas'))
-        <em>{{ $errors->first('num_pessoas') }}</em>
+    <em>{{ $errors->first('num_pessoas') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -136,7 +137,7 @@
         name="conta_horas_inicio" id="conta_horas_inicio"
         placeholder="CHInicial" value="@if(isset($movimento)){{ old('conta_horas_inicio', $movimento->conta_horas_inicio) }}@else{{ old('conta_horas_inicio') }}@endif" />
     @if ($errors->has('conta_horas_inicio'))
-        <em>{{ $errors->first('conta_horas_inicio') }}</em>
+    <em>{{ $errors->first('conta_horas_inicio') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -146,7 +147,7 @@
         name="conta_horas_fim" id="conta_horas_fim"
         placeholder="CHFinal" value="@if(isset($movimento)){{ old('conta_horas_fim', $movimento->conta_horas_fim) }}@else{{ old('conta_horas_fim') }}@endif" />
     @if ($errors->has('conta_horas_fim'))
-        <em>{{ $errors->first('conta_horas_fim') }}</em>
+    <em>{{ $errors->first('conta_horas_fim') }}</em>
     @endif
 </div>
 <div class="form-group">
@@ -206,59 +207,59 @@
         name="num_recibo" id="inputNRecibo"
         placeholder="NRecibo" value="@if(isset($movimento)){{ old('num_recibo', $movimento->num_recibo) }}@else{{ old('num_recibo') }}@endif" />
     @if ($errors->has('num_recibo'))
-        <em>{{ $errors->first('num_recibo') }}</em>
+    <em>{{ $errors->first('num_recibo') }}</em>
     @endif
 </div>
 <div class="form-group">
     <label for="inputObs">Observações</label>
     <textarea class="form-control"
-        name="observacoes">
+              name="observacoes">
          @if(isset($movimento)){{ old('observacoes', $movimento->observacoes) }}@else{{ old('observacoes') }}@endif </textarea>
     @if ($errors->has('observacoes'))
-        <em>{{ $errors->first('observacoes') }}</em>
+    <em>{{ $errors->first('observacoes') }}</em>
     @endif
 </div>
 <div id="instrucao_form">
-<div class="form-group">
-    <label for="inputTInstrucao">Tipo de Instrução</label>
-    <div class='radio' name='tipo_instrucao'>
-        @if(isset($movimento))
-        <div>
-            {!! Form::radio('tipo_instrucao','D', $movimento->tipo_instrucao == 'D')  !!}
-            <label class="form-check-label">Duplo Comando</label>
+    <div class="form-group">
+        <label for="inputTInstrucao">Tipo de Instrução</label>
+        <div class='radio' name='tipo_instrucao'>
+            @if(isset($movimento))
+            <div>
+                {!! Form::radio('tipo_instrucao','D', $movimento->tipo_instrucao == 'D')  !!}
+                <label class="form-check-label">Duplo Comando</label>
+            </div>
+            <div>
+                {!! Form::radio('tipo_instrucao','S',$movimento->tipo_instrucao == 'S')  !!}
+                <label class="form-check-label">Solo</label>
+            </div>
+            @else
+            <div>
+                {!! Form::radio('tipo_instrucao','D')  !!}
+                <label class="form-check-label">Duplo Comando</label>
+            </div>
+            <div>
+                {!! Form::radio('tipo_instrucao','S')  !!}
+                <label class="form-check-label">Solo</label>
+            </div>
+            @endif
         </div>
-        <div>
-            {!! Form::radio('tipo_instrucao','S',$movimento->tipo_instrucao == 'S')  !!}
-            <label class="form-check-label">Solo</label>
-        </div>
-        @else
-        <div>
-            {!! Form::radio('tipo_instrucao','D')  !!}
-            <label class="form-check-label">Duplo Comando</label>
-        </div>
-        <div>
-            {!! Form::radio('tipo_instrucao','S')  !!}
-            <label class="form-check-label">Solo</label>
-        </div>
+    </div>
+    <div class="form-group">
+        <label for="inputInstrutor">Instrutor Id</label>
+        <input
+            type="number" class="form-control"
+            name="instrutor_id" id="inputInstrutor"
+            placeholder="Instrutor" value="@if(isset($movimento)){{ old('instrutor_id', $movimento->instrutor_id) }}@else{{ old('instrutor_id') }}@endif" />
+        @if ($errors->has('instrutor_id'))
+        <em>{{ $errors->first('instrutor_id') }}</em>
         @endif
     </div>
-</div>
-<div class="form-group">
-    <label for="inputInstrutor">Instrutor Id</label>
-    <input
-        type="number" class="form-control"
-        name="instrutor_id" id="inputInstrutor"
-        placeholder="Instrutor" value="@if(isset($movimento)){{ old('instrutor_id', $movimento->instrutor_id) }}@else{{ old('instrutor_id') }}@endif" />
-    @if ($errors->has('instrutor_id'))
-        <em>{{ $errors->first('instrutor_id') }}</em>
-    @endif
-</div>
 </div>
 <div id="form-conflito" style="display: none;">
     <div class="form-group">
         <label for="justificacao_conflito">Justificação</label>
         <textarea class="form-control"
-        name="justificacao_conflito">
+                  name="justificacao_conflito">
             @if(isset($movimento)){{ old('justficacao_conflito', $movimento->justificacao_conflito) }}@else{{ old('justficacao_conflito') }}@endif </textarea>
     </div>
 </div>
@@ -302,26 +303,26 @@
         }
     }
     $(function() {
-      $("#natureza").change(function() {
-        var val = $(this).val();
-        if (val === "I") {
-          $("#instrucao_form").show();
-        } else {
-          $("#instrucao_form").hide();
-        }
-      }).trigger('change');
-    
+        $("#natureza").change(function() {
+            var val = $(this).val();
+            if (val === "I") {
+                $("#instrucao_form").show();
+            } else {
+                $("#instrucao_form").hide();
+            }
+        }).trigger('change');
+
         var val = $("#conflito").val();
         if (val == 1) {
-          let confirmation = confirm("Os valores dos conta-horas estão com conflitos. Deseja continuar?");
-          if(confirmation){
-            $("#form-conflito").show();
-        } else {
-            $("#form-conflito").hide();
-            $("#conta_horas_inicio").val("");
-            
+            let confirmation = confirm("Os valores dos conta-horas estão com conflitos. Deseja continuar?");
+            if(confirmation){
+                $("#form-conflito").show();
+            } else {
+                $("#form-conflito").hide();
+                $("#conta_horas_inicio").val("");
 
-        }
+
+            }
         }
 
 
