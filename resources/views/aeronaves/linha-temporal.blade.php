584483f292b1e70da8a91b6dea37888175871a25
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-06-01 22:24:50 +0100

    Update linha-temporal.blade.php

--- resources/views/aeronaves/linha-temporal.blade.php
+++ resources/views/aeronaves/linha-temporal.blade.php
@@ -1,77 +1,82 @@
 @extends('master')
 @section('title', "Linhas Temporais")
 @section('content')
-<div class="container" style="text-align: center; padding-top: 10px">
-	<h3>{{$title}}</h3>
-	<br>
-	<h5>Filtre por aeronave e pelo intervalo de data desejado</h5>
-</div>
-@if(session('unsuccess'))
-    @include('partials.unsuccess')
-@endif
-<div class="col-md-4 container" >
-    <div class="dropdown" style="padding-top: 10px" >
-        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" style="width: 100%"><i class="fas fa-filter"></i>
-            Filtros
-        </button>
-        <div class="dropdown-menu" style="width: 100%">
-        <form  method="GET" action="{{action('AeronaveController@linhaTemporal')}}" id="pesquisarMovimento">
-            <div class="form-group ">
-                    {{ Form::select('matricula', $aeronaves, null, ['class' => 'form-control conta-horas', 'name' => 'matricula'])}}
-            </div>
-            <div class="form-group">
-            	<label for="data_inf"> De:</label>
-                <input type="date" class="form-control" name="data_inf">
-            </div>
-            <div class="form-group">
-            	<label for="data_sup"> A:</label>
-                <input type="date" class="form-control" name="data_sup">
-            </div>
-            <div class="col-auto">
-                <button type="submit" class="btn btn-sm btn-success mb-3" style="width: 100%">
-                    <i class="fas fa-search"></i> Pesquisar
-                </button>
-            </div>
-        </form>
-        </div>
+    <div class="container" style="text-align: center; padding-top: 10px">
+        <h3>{{$title}}</h3>
+        <br>
+        <h5>Filtre por aeronave e pelo intervalo de data desejado</h5>
     </div>
-</div>
-<br>
-
-@if(count($datas))
-<div class="container" >
-	<div class="container" style="text-align: center">
-		<h5>{{$dataMaisRecente}}</h5>
-	</div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="main-timeline">
-            	@for($i=0; $i<$diferenca+1;$i++)
-                <div class="timeline" >
-	                    <a class="timeline-content">
-	                        <h3 class="title">@if(isset($datas[$i])){{$datas[$i]}}@else Sem registos @endif</h3>
-	                        <p class="description">
-	                        	@foreach($movimentosAeronave as $key => $movimento)
-	                        		@if(isset($datas[$i]) && $movimento==$datas[$i])
-	                        			Movimento {{$key}}
-	                        			<br>
-	                        		@endif
-	                        	@endforeach
-	                        </p>
-	                    </a>
-	                </div>
-                @endfor
+    @if(session('unsuccess'))
+        @include('partials.unsuccess')
+    @endif
+    <div class="col-md-4 container" >
+        <div class="dropdown" style="padding-top: 10px" >
+            <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" style="width: 100%"><i class="fas fa-filter"></i>
+                Filtros
+            </button>
+            <div class="dropdown-menu" style="width: 100%">
+                <form  method="GET" action="{{action('AeronaveController@linhaTemporal')}}" id="pesquisarMovimento">
+                    <div class="form-group ">
+                        {{ Form::select('matricula', $aeronaves, null, ['class' => 'form-control conta-horas', 'name' => 'matricula'])}}
+                    </div>
+                    <div class="form-group">
+                        <label for="data_inf"> De:</label>
+                        <input type="date" class="form-control" name="data_inf">
+                    </div>
+                    <div class="form-group">
+                        <label for="data_sup"> A:</label>
+                        <input type="date" class="form-control" name="data_sup">
+                    </div>
+                    <div class="col-auto">
+                        <button type="submit" class="btn btn-sm btn-success mb-3" style="width: 100%">
+                            <i class="fas fa-search"></i> Pesquisar
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-    <div class="container" style="text-align: center">
-		<h5>{{$dataMaisAntiga}}</h5>
-	</div>
-</div>
-@else
-<div class="container" style="text-align: center">
-    <h5>Nenhum movimento encontrado nessas datas.</h5>
+    <br>
 
-</div>
- @endif
+    @if(count($datas))
+        <div class="container" >
+            <div class="container" style="text-align: center">
+                <h5>{{$dataMaisRecente}}</h5>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="main-timeline">
+                        @for($i=0; $i<$diferenca+1;$i++)
+                            <div class="timeline" >
+                                <a class="timeline-content">
+                                    <h3 class="title">@if(isset($datas[$i])){{$datas[$i]}}@else Sem registos @endif</h3>
+                                    <p class="description">
+                                        @foreach($movimentosAeronave as $key => $movimento)
+                                            @if(isset($datas[$i]) && $movimento==$datas[$i])
+                                                Movimento {{$key}}<form method="GET" action="{{action('MovimentoController@edit', $key)}}" role="form" class="inline">
+                                        @csrf
+                                        @method('get')
+                                        <input type="hidden" name="movimento_id" value="{{ $key }}">
+                                        {!! Form::button('<i class="fas fa-exclamation-triangle"></i> Editar', ['type' => 'submit', 'class' => 'btn btn-primary', 'style'=>"width:100%"]) !!}
+                                    </form>
+                                    <br>
+                                    @endif
+                                    @endforeach
+                                    </p>
+                                </a>
+                            </div>
+                        @endfor
+                    </div>
+                </div>
+            </div>
+            <div class="container" style="text-align: center">
+                <h5>{{$dataMaisAntiga}}</h5>
+            </div>
+        </div>
+    @else
+        <div class="container" style="text-align: center">
+            <h5>Nenhum movimento encontrado nessas datas.</h5>
+
+        </div>
+    @endif
 @endsection
