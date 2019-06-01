ce3e7d0ab791ba68caa189404a9e0b8d5f8fe39f
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-06-01 22:25:56 +0100

    Update errors.blade.php

--- resources/views/partials/errors.blade.php
+++ resources/views/partials/errors.blade.php
@@ -1,7 +1,7 @@
 <div class="alert alert-danger" style="margin-top: 2vh">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
     <ul>
-
+	{{session('success')}}
     @foreach ($errors->all() as $error)
         <li>{{$error}}</li>
     @endforeach
