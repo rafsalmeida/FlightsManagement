0decd653ace81c7c0f81f67f0fcc78cff5bcc000
rafsalmeida <50708203+rafsalmeida@users.noreply.github.com>
2019-06-01 22:24:36 +0100

    Update add.blade.php

--- resources/views/aeronaves/add.blade.php
+++ resources/views/aeronaves/add.blade.php
@@ -3,53 +3,54 @@
 @section('content')
 @include('aeronaves.partials.add-edit')
 @if (count($errors) > 0)
-    @include('partials.errors')
+@include('partials.errors')
 @endif
 <form method="POST" action="{{ action('AeronaveController@store')}}"  class="form-group">
-  @csrf
+    @csrf
     <div class="container" style="padding-top: 15px">
-      @yield('form')
+        @yield('form')
     </div>
     <div class="w-100 p-3  container" >
-       <table class="table table-bordered  table table-sm shadow p-3 mb-5 bg-white rounded">
-         <thead class="thead-light">
-        <tr>
-            <th>Unidade Conta Horas</th>
-            <th>Minutos</th>
-            <th>Preco</th>
-        </tr>
-        </thead>
-        <tbody>
-          @for ($i = 1; $i < 11; $i++)
+        <table class="table table-bordered  table table-sm shadow p-3 mb-5 bg-white rounded">
+            <thead class="thead-light">
+            <tr>
+                <th>Unidade Conta Horas</th>
+                <th>Minutos</th>
+                <th>Preco</th>
+            </tr>
+            </thead>
+            <tbody>
+            @for ($i = 1; $i < 11; $i++)
             <tr>
                 <td>{{$i}}</td>
                 <td>
-                  <div class="form-group">
-                    <input type="text" class="form-control" name="tempos[{{$i}}]" id="minuto"
-                      placeholder="Minutos"/>
-                           <!-- Verificar-->
-                      @if ($errors->has('tempos'))
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="tempos[{{$i}}]" id="minuto"
+                               placeholder="Minutos"/>
+                        <!-- Verificar-->
+                        @if ($errors->has('tempos'))
                         <em>{{ $errors->first('tempos') }}</em>
-                      @endif
-                  </div>
+                        @endif
+                    </div>
                 </td>
                 <td>
-                  <div class="form-group">
-                    <input type="text" class="form-control" name="precos[{{$i}}]" id="preco_minuto"
-                          placeholder="Precos"/>
-                               <!-- Verificar-->
-                               @if ($errors->has('precos'))
-                          <em>{{ $errors->first('precos') }}</em>
-                         @endif
-                  </div> 
-                </td> 
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="precos[{{$i}}]" id="preco_minuto"
+                               placeholder="Precos"/>
+                        <!-- Verificar-->
+                        @if ($errors->has('precos'))
+                        <em>{{ $errors->first('precos') }}</em>
+                        @endif
+                    </div>
+                </td>
             </tr>
-           @endfor
-        </tbody>
-    </table>
-    <div class="form-group">
-        <button type="submit" class="btn btn-success" name="ok">Adicionar</button>
-        <button type="submit" class="btn btn-default" name="cancel">Cancelar</button>
+            @endfor
+            </tbody>
+        </table>
+        <div class="form-group">
+            <button type="submit" class="btn btn-success" name="ok">Adicionar</button>
+            <button type="submit" class="btn btn-default" name="cancel">Cancelar</button>
+        </div>
     </div>
 </form>
 @endsection
